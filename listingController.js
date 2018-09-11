040a1687f93fb6442b11c2bacd97c24de1610209
jaspera11 <33080050+jaspera11@users.noreply.github.com>
2018-09-10 23:44:00 -0400

    Add files via upload

--- listingController.js
+++ listingController.js
@@ -7,8 +7,23 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+	var name, code, latitude, longitude, address;
+	name = document.getElementById("name");
+	code = document.getElementById("code");
+	latitude = document.getElementById("latitude");
+	longitude = document.getElementById("longitude");
+	address = document.getElementById("address");
+	
+	//$scope.listings.push({"code": code,\n"name": name,\n"coordinates": {\n"latitude": latitude,\n"longitude": longitude\n},\n"address": address});
+    	
+    };
+    $scope.deleteListing = function($index) {
+	Listings.splice($index,1);
+    };
+    $scope.showDetails = function($index) {
+	$scope.detailedInfo = Listings[$index];
+	console.log($scope.detailedInfo);
+    };
   }
 ]);
