ddb86b501531b194a3de7899dbc70b0de68e17bd
Alexander Jasper <jaspera11@ufl.com>
2018-09-17 15:05:21 -0400

    Assignment3 Submission

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -4,17 +4,22 @@
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config.js'),
+    listings = require('./listings.json');
 
 /* Connect to your database */
+mongoose.connect(config.db.uri);
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
+for (var i = 0; i < listings.entries.length; i++) {
+    new Listing(listings.entries[i]).save();
+}
 
 
 /* 
